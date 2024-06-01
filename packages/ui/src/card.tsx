2132e2814f1afb00af2092a4bfefd72f364a542e
Dhruv Garala <dhruvgarala@Dhruvs-MacBook-Air.local>
2024-06-01 18:14:03 +0530

    my changets

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -12,6 +12,7 @@ export function Card({
       className="border p-6 bg-white rounded-xl bg-[#ededed]"
     >
       <h1 className="text-xl border-b pb-2">
+        <h1>helloid;jsrdhbfsndkcswaklejf;ndcwjke</h1>
         {title}
       </h1>
       <p>{children}</p>
